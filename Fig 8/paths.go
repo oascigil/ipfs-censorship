097d18c0fa0ce8edba369e09bc10f6652ba1fa73
navinkeizer <46092420+navinkeizer@users.noreply.github.com>
2023-07-04 12:04:19 +0100

    Update paths.go

--- Fig 8/paths.go
+++ Fig 8/paths.go
@@ -1,6 +1,5 @@
 package main
 
-const DHT_CLIENT_PATH = "./DHT_Client/DHTClient"
 const IPFS_PATH = "../../common/kubo/cmd/ipfs/ipfs"
 const SYBIL_NODE_PATH = "../../common/Sybil_DHT_Nodes/SybilNode"
 const LOGS_PATH = "./logs/"
